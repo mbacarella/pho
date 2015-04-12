9ba277807ff955ab349bcf83a9e71b61bb1890f1
Michael Bacarella <mbac@panix.com>
2015-04-12 19:00:49 +0100

    rudimentary tree nav

--- src/pho/routes/home.clj
+++ src/pho/routes/home.clj
@@ -1,10 +1,14 @@
 (ns pho.routes.home
   (:require [pho.layout :as layout]
+            [pho.tree :as tree]
             [compojure.core :refer [defroutes GET]]
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render "home.html"))
+  (let [[sets photos] (tree/get-sets-and-photos "/home/mbac/photos")]
+    (layout/render "home.html"
+                   {:sets sets
+                    :photos photos})))
 
 (defroutes home-routes
   (GET "/" [] (home-page)))
