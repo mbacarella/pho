0c1575c634320105032b674e6b3c5a7bd7f61e15
Michael Bacarella <mbac@panix.com>
2015-04-12 17:27:59 +0100

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/pho.jar clojure.main -m pho.core
---
+++ test/pho/test/handler.clj
@@ -0,0 +1,13 @@
+(ns pho.test.handler
+  (:use clojure.test
+        ring.mock.request
+        pho.handler))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
+
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
