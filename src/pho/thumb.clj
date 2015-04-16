f67d939ceb4a49cfba7c07953ed4b386557221c7
Michael Bacarella <mbac@panix.com>
2015-04-16 10:44:00 +0100

    forgot to git add

---
+++ src/pho/thumb.clj
@@ -0,0 +1,16 @@
+(ns pho.thumb
+  (:require [clojure.java.io :as io])
+  (:import  [javax.imageio.ImageIO]
+            [java.awt.image.BufferedImage]))
+
+(defn convert-to-png-and-resize [file-in file-out new-width]
+  (let [img         (javax.imageio.ImageIO/read (io/file file-in))
+        orig-width  (.getWidth img)
+        orig-height (.getHeight img)
+        new-height  (* orig-height (/ new-width orig-width))
+        imgtype     (java.awt.image.BufferedImage/TYPE_INT_ARGB)
+        simg        (java.awt.image.BufferedImage. new-width new-height imgtype)
+        g           (.createGraphics simg)]
+    (.drawImage g img 0 0 new-width new-height nil)
+    (.dispose g)
+    (javax.imageio.ImageIO/write simg "png" (io/file file-out))))
